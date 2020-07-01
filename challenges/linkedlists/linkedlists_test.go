067f89af9c69d9b2a8dd6993fa9147c3e7ab9035
Dathan Vance Pattishall <dathanvp@gmail.com>
2020-06-30 17:44:40 -0700

    fixed remove from the end.

--- challenges/linkedlists/linkedlists_test.go
+++ challenges/linkedlists/linkedlists_test.go
@@ -1,8 +1,11 @@
 package challenges_test
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 /**
@@ -41,13 +44,24 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 
 	removeThis := head
 	moveRight := len - n
+	fmt.Printf("Len: %d Moving Right: %d\n", len, moveRight)
 	// going from left to right N spaces
 	for i := 1; i <= moveRight; i++ {
 		removeThis = removeThis.Next
 	}
+	// deleteting the end (need to back up one)
+	if removeThis != nil && removeThis.Next == nil && removeThis != head {
+		removeThis = head
+		moveRight := len - n - 1
+		for i := 1; i <= moveRight; i++ {
+			removeThis = removeThis.Next
+		}
+		removeThis.Next = nil
+		return head
+	}
 
 	// 1->2->3->4->5: if n == 2; 5-2 == 3
-	if removeThis != nil && removeThis.Next == nil {
+	if removeThis != nil && removeThis.Next == nil && removeThis == head {
 		return nil
 	}
 
@@ -55,6 +69,38 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	deleteNode(removeThis)
 	return head
 
+}
+
+func Test_removeNthFromEnd(t *testing.T) {
+	type args struct {
+		head *ListNode
+		n    int
+	}
+
+	n1 := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
+	n2 := &ListNode{1, nil}
+	n3 := &ListNode{1, &ListNode{2, nil}}
+	n4 := &ListNode{1, &ListNode{2, &ListNode{3, nil}}}
+
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{"remove", args{head: n1, n: 2}, n1},
+		{"remove", args{head: n2, n: 1}, nil},
+		{"remove", args{head: n3, n: 1}, n3},
+		{"remove", args{head: n4, n: 1}, n4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeNthFromEnd(tt.args.head, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeNthFromEnd() = %v, want %v", got, tt.want)
+			} else {
+				t.Log(spew.Sprintf("OK: %v\n", tt.want))
+			}
+		})
+	}
 }
 
 func Test_deleteNode(t *testing.T) {
@@ -73,34 +119,6 @@ func Test_deleteNode(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			deleteNode(tt.args.node)
-		})
-	}
-}
-
-func Test_removeNthFromEnd(t *testing.T) {
-	type args struct {
-		head *ListNode
-		n    int
-	}
-
-	n := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
-
-	n2 := &ListNode{1, nil}
-
-	tests := []struct {
-		name string
-		args args
-		want *ListNode
-	}{
-		// TODO: Add test cases.
-		{"remove", args{head: n, n: 2}, n},
-		{"remove", args{head: n2, n: 1}, nil},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := removeNthFromEnd(tt.args.head, tt.args.n); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("removeNthFromEnd() = %v, want %v", got, tt.want)
-			}
 		})
 	}
 }
